8d9e6f8cb36c0a9fa19d5b6d7a8605d51ec4170f
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2021-08-15 21:29:30 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
   session_start(); 
 
   return [
-          'host' => $_SERVER["HOST"],
-	  'porta' => $_SERVER["PORTA"],
-	  'banco' => $_SERVER["BANCO"],
-	  'usuario' => $_SERVER["USUARIO"],
-	  'senha' => $_SERVER["SENHA"],
-	  'recaptcha' => $_SERVER["RECAPTCHA"]
+          'host' => $_ENV["HOST"],
+	  'porta' => $_ENV["PORTA"],
+	  'banco' => $_ENV["BANCO"],
+	  'usuario' => $_ENV["USUARIO"],
+	  'senha' => $_ENV["SENHA"],
+	  'recaptcha' => $_ENV["RECAPTCHA"]
   ]; 
 ?>
