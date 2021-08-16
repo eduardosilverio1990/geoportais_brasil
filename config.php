49569077142ba600533bfcea91035acc8e8ec792
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2021-08-15 21:05:45 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,9 +7,9 @@
   //$dotenv->load();
 
   return [
-          'host' => getenv("HOST"),
-	  'porta' => getenv("PORTA"),
-	  'banco' => getenv("BANCO"),
+          'host' => getenv("POSTGRES_HOST"),
+	  'porta' => getenv("POSTGRES_PORT"),
+	  'banco' => getenv("POSTGRES_DATABASE"),
 	  'usuario' => getenv("POSTGRES_USER"),
 	  'senha' => getenv("POSTGRES_PASSWORD"),
 	  'recaptcha' => getenv("RECAPTCHA")
