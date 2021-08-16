ee438d6f178fecebfab455e65b5fc2e44efb7218
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2021-08-15 22:11:15 -0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 	  'porta' => $_ENV["POSTGRES_PORT"],
 	  'banco' => $_ENV["POSTGRES_DATABASE"],
 	  'usuario' => $_ENV["POSTGRES_USER"],
-	  'senha' => $_ENV["POSTGRES_PASWORD"],
+	  'senha' => $_ENV["POSTGRES_PASSWORD"],
 	  'recaptcha' => $_ENV["RECAPTCHA_SITEKEY"]
   ]; 
 ?>
